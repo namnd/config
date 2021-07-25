31da4b4ed8677752431dd5745c5de66a01dc0bee
Nam Nguyen <nnguyen@pay.com.au>
2021-07-25 10:48:20 +1000

    migrate vim to lua config

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,6 +1,22 @@
+require('settings')
 require('plugins')
 
 vim.cmd('colorscheme tender')
 
 require('setup')
 require('bindings')
+
+vim.cmd [[
+set undodir=~/.vim/undodir undofile
+set noswapfile nobackup nowritebackup
+
+nmap <leader>p "0p
+nmap <leader>P "0P
+
+let g:dirvish_mode = ':sort ,^.*[\/],'
+
+autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 70})
+autocmd WinEnter * set colorcolumn=81 cursorline cursorcolumn
+autocmd WinLeave * set colorcolumn=0 nocursorline nocursorcolumn
+autocmd VimResized * :wincmd =
+]]
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,19 @@
+vim.o.clipboard = 'unnamed'
+vim.o.mouse = 'nv'
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.wo.list = true
+vim.o.listchars = 'tab:|_,trail:·,eol:↵'
+vim.o.completeopt = 'menuone,noselect'
+vim.o.shortmess = vim.o.shortmess .. 'c'
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+
+vim.g.mapleader = ','
