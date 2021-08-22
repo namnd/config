e020e169677bbe722b8262b83a15596b5660ee8b
Nam Nguyen <nnguyen@pay.com.au>
2021-08-22 22:55:50 +1000

    tpope vim repeat

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,6 +7,7 @@ return require('packer').startup(function()
   use 'tpope/vim-commentary'
   use 'tpope/vim-surround'
   use 'tpope/vim-fugitive'
+  use 'tpope/vim-repeat'
   use {
     'shadmansaleh/lualine.nvim',
     requires = {'kyazdani42/nvim-web-devicons', opt = true}
