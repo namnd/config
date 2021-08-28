62008c27884956d61ff8575af1582753869e0655
Nam Nguyen <nnguyen@pay.com.au>
2021-08-28 20:34:03 +1000

    switch Brave to Chrome

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,9 +2,10 @@ local hyper = {'ctrl', 'alt', 'cmd', 'shift'}
 local miniHyper = {'cmd', 'shift'}
 
 hs.hotkey.bind(hyper, "1", function() print(hs.application.launchOrFocus('kitty')) end)
-hs.hotkey.bind(hyper, "2", function() print(hs.application.launchOrFocus('Brave Browser')) end)
+hs.hotkey.bind(hyper, "2", function() print(hs.application.launchOrFocus('Google Chrome')) end)
 hs.hotkey.bind(hyper, "3", function() print(hs.application.launchOrFocus('Slack')) end)
 hs.hotkey.bind(hyper, "0", function() print(hs.application.launchOrFocus('Finder')) end)
+hs.hotkey.bind(hyper, "9", function() print(hs.application.launchOrFocus('Brave Browser')) end)
 hs.hotkey.bind(hyper, "r", function() hs.reload() end)
 
 hs.alert.show("Config reload")
