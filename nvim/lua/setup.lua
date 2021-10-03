2d8d2d78f885417d209ef139ad22551bb38e124f
Nam Nguyen <nnguyen@pay.com.au>
2021-10-03 21:11:33 +1100

    clangd C lsp

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -32,7 +32,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = {'tsserver', 'terraformls', 'pyright'}
+local servers = {'tsserver', 'terraformls', 'pyright', 'clangd'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
