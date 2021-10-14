24fa90a882c3666a3ac97f0dc103e7e436135bb2
Nam Nguyen <nnguyen@pay.com.au>
2021-10-14 22:54:05 +1100

    git file type folding

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -28,4 +28,5 @@ autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 70}
 autocmd WinEnter * set colorcolumn=81 cursorline cursorcolumn
 autocmd WinLeave * set colorcolumn=0 nocursorline nocursorcolumn
 autocmd VimResized * :wincmd =
+autocmd FileType git,gitcommit setlocal foldmethod=syntax foldenable
 ]]
