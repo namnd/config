4992c2bc5e67fca4dca8348a28425ecaffdcf315
Nam Nguyen <nam.nguyen@indebted.co>
2022-05-22 18:17:42 +1000

    refactor emacs config

---
+++ emacs/init.el
@@ -0,0 +1,27 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+(set-fringe-mode 10)
+
+(set-face-attribute 'default nil :font "NanumGothicCoding" :height 200)
+
+(load-theme 'wombat)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
