bd319106d013c2badb975bd1f40f4c851165b8f6
Nam Nguyen <nam.nguyen@indebted.co>
2022-05-26 19:58:04 +1000

    add keelan

--- nvim/snippets/gitcommit.lua
+++ nvim/snippets/gitcommit.lua
@@ -3,4 +3,5 @@ return {
   parse("bruno", "Bruno Pinheiro <bruno.barbosa@indebted.co>"),
   parse("roscoe", "Roscoe McGarry <roscoe.mcgarry@indebted.co>"),
   parse("regio", "Regio Michelin <regio.michelin@indebted.co>"),
+  parse("keelan", "Keelan Pool <keelan.pool@indebted.co>"),
 }
