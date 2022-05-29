39ced6b9032a5455d13eee49facb5f5c0de8921f
Nam Nguyen <nam.nguyen@indebted.co>
2022-05-29 20:21:24 +1000

    remove pii

--- nvim/snippets/gitcommit.lua
+++ nvim/snippets/gitcommit.lua
@@ -1,7 +1,3 @@
 return {
   parse("Co", "Co-authored-by:"),
-  parse("bruno", "Bruno Pinheiro <bruno.barbosa@indebted.co>"),
-  parse("roscoe", "Roscoe McGarry <roscoe.mcgarry@indebted.co>"),
-  parse("regio", "Regio Michelin <regio.michelin@indebted.co>"),
-  parse("keelan", "Keelan Pool <keelan.pool@indebted.co>"),
 }
