ac94b1a81770a001b226192ab5893698fb37a51e
Nam Nguyen <nam.nguyen@indebted.co>
2022-06-02 13:10:04 +1000

    setup basic org roam

--- emacs/init.el
+++ emacs/init.el
@@ -63,6 +63,20 @@
 (define-key evil-normal-state-map (kbd "zc") 'org-hide-entry)
 (define-key evil-normal-state-map (kbd "zo") 'org-show-entry)
 
+(use-package org-roam
+  :ensure t
+  :init
+  :custom
+  (org-roam-directory "~/notes/roam")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point))
+  :config
+  (org-roam-setup))
+
 ;; Autocompletion
 (use-package vertico
   :ensure t
