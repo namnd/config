475e0142464b6a20325940298e34e9fe975b33d5
Nam Nguyen <nam.nguyen@indebted.co>
2022-06-04 21:14:18 +1000

    yank emacs to system clipboard

--- emacs/init.el
+++ emacs/init.el
@@ -40,6 +40,14 @@
   :config
   (evil-mode 1))
 
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+
 (use-package evil-surround
   :ensure t
   :config
