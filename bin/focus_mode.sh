48e36f7f8af65af72624b6cd2832f92e78c3979e
Nam Nguyen <nam.nguyen@indebted.co>
2022-06-14 20:59:54 +1000

    remove qutebrowser by Vieb

--- bin/focus_mode.sh
+++ bin/focus_mode.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 kitty_id=$(yabai -m query --windows | jq -re '.[] | select(."app" == "kitty") | .id')
-qute_id=$(yabai -m query --windows | jq -re '.[] | select(."app" == "qutebrowser") | .id')
+web_id=$(yabai -m query --windows | jq -re '.[] | select(."app" == "Vieb") | .id')
 
-if [[ $kitty_id && $qute_id ]]; then
+if [[ $kitty_id && $web_id ]]; then
   # Move others to secondary
   s1=$(yabai -m query --windows --space 1 | jq -re '.[] | .id')
   for w in $s1
   do
-    if [ "$w" != "$kitty_id" ] && [ "$w" != "$qute_id" ]; then
+    if [ "$w" != "$kitty_id" ] && [ "$w" != "$web_id" ]; then
       yabai -m window $w --space 2
     fi
   done
   # Make sure kitty and qutebrowser in primary
   yabai -m window $kitty_id --space 1
-  yabai -m window $qute_id --space 1
+  yabai -m window $web_id --space 1
   yabai -m config --space 1 layout bsp
   yabai -m window $kitty_id --swap east
 fi
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,7 +3,7 @@ local modifier = {'ctrl', 'shift'}
 
 local apps = {
   ["1"] = "kitty",
-  ["2"] = "qutebrowser",
+  ["2"] = "Vieb",
   ["3"] = "Slack",
   ["4"] = "zoom.us",
   ["0"] = "Finder",
