18e627d48de794491326915b69c4f60028250bb4
Nam Nguyen <nam.nguyen@indebted.co>
2022-06-26 23:44:47 +1000

    nvim treesitter textobjects keybinding

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -64,6 +64,7 @@ require('nvim-treesitter.configs').setup ({
       lookahead = true,
       keymaps = {
         ["if"] = "@function.inner",
+        ["af"] = "@function.outer",
       },
     },
     swap = {
@@ -73,6 +74,22 @@ require('nvim-treesitter.configs').setup ({
       },
       swap_previous = {
         ["vN"] = "@parameter.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
       },
     },
   },
