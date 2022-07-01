9d0c67367842ddf31c8edd1ec93d4f178e2729cb
Nam Nguyen <nam.nguyen@indebted.co>
2022-07-01 12:49:36 +1000

    zig lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -17,7 +17,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local servers = {'tsserver', 'terraformls', 'clangd', 'rnix'}
+local servers = {'tsserver', 'terraformls', 'clangd', 'rnix', 'zls'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
