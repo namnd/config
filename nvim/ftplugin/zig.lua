3ee71f319e93fee2fc12e9c239f3176e308c7246
Nam Nguyen <nam.nguyen@indebted.co>
2022-07-02 14:46:10 +1000

    zig ftplugin

--- nvim/ftplugin/zig.lua
+++ nvim/ftplugin/zig.lua
@@ -1,3 +1,15 @@
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.o.smartindent = true
+
+vim.keymap.set("n", "<leader>tt", ":Dispatch zig test %<cr>")
+
+
+local group = vim.api.nvim_create_augroup("ZigGroup", { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = group,
+  callback = function ()
+    vim.lsp.buf.formatting_sync({}, 1000)
+  end
+})
