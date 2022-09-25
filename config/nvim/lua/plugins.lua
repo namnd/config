85d665bcd01815e0769825f2882fa4dab3d110cf
Nam Nguyen <nam.nguyen@indebted.co>
2022-09-25 16:07:56 +1000

    nvim(plugin): treesitter context

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -33,6 +33,7 @@ return require('packer').startup(function(use)
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use 'nvim-treesitter/playground'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'nvim-treesitter/nvim-treesitter-context'
 
   -- lsp
   use 'neovim/nvim-lspconfig'
