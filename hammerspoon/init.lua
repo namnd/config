a76f969b1972a75e53b729e96dc35d45d571cecd
Nam Nguyen <nam.nguyen@indebted.co>
2022-10-17 07:33:42 +1100

    chore(hammerspoon): clean up custom key

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,37 +1,15 @@
-local miniModifier = {'cmd', 'shift'}
 local modifier = {'ctrl', 'shift'}
 
 local apps = {
   ["1"] = "kitty",
   ["2"] = "qutebrowser",
   ["3"] = "Slack",
-  ["4"] = "zoom.us",
   ["0"] = "Finder",
 }
 for k,v in pairs(apps) do
   hs.hotkey.bind(modifier, k, function() print(hs.application.launchOrFocus(v)) end)
 end
 hs.hotkey.bind(modifier, "r", function() hs.reload() end)
-hs.hotkey.bind(modifier, "9", function()
-  local allWindows = hs.window.allWindows()
-  for _, win in pairs(allWindows) do
-    local x = win:application():name()
-    if x == "emacs" then
-      win:maximize()
-      win:focus()
-    end
-  end
-end)
-
-function MoveToOtherSpace()
-  hs.execute("~/dotfiles/bin/move_to_other_space.sh", true)
-end
-hs.hotkey.bind(miniModifier, "m", function() MoveToOtherSpace() end)
-
-function FocusMode()
-  hs.execute("~/dotfiles/bin/focus_mode.sh", true)
-end
-hs.hotkey.bind(miniModifier, "f", function() FocusMode() end)
 
 -- Install spoons
 hs.loadSpoon("SpoonInstall")
