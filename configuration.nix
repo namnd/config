c145015b1d48e90b54ba2634f00a0b093f70d98b
Nam Nguyen <nam.nguyen@indebted.co>
2022-10-23 06:47:30 +1100

    init nixos configuration

---
+++ configuration.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Australia/Brisbane";
+  i18n.defaultLocale = "en_AU.utf8";
+
+  services.xserver = {
+    layout = "au";
+    xkbVariant = "";
+  };
+
+  users.users.namnd = {
+    isNormalUser = true;
+    description = "Nam Nguyen";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqsFOesSlXn6hnDo/zVO4znA/vpd7oXdnu9qcEb3xHf" ];
+ 
+    packages = with pkgs; [];
+  };
+
+  virtualisation.docker.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
+  services.openssh.enable = true;
+
+  system.stateVersion = "22.05";
+}
