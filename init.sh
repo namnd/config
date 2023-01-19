58f5db6a6f822cf91bd14b7f787b9867c096be48
Nam Nguyen <me@namnd.com>
2023-01-19 21:41:11 +1100

    fix order home manager config

--- init.sh
+++ init.sh
@@ -4,9 +4,24 @@ if [ "$1" ]; then
   sed -i "s/userEmail/$1/g" $HOME/dotfiles/gitconfig
 fi
 
+# install Nix (2.8.1) for multi user (for Mac only)
+if [ ! -x "$(command -v nix)" ]; then
+  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.8.1/install)" --daemon
+fi
+
+# install Home manager if not yet installed
+if [ ! -x "$(command -v home-manager)" ]; then
+  mkdir -p /nix/var/nix/profiles/per-user/root/channels
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+  nix-channel --update
+  export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+  nix-shell '<home-manager>' -A install
+fi
+
 apps=(
   "gitconfig"
   "gitignore"
+  "zshrc"
 )
 config=(
   "nvim"
@@ -35,25 +50,5 @@ do
   rm -rf $HOME/.config/${c}
   ln -sfn $PWD/config/${c} $HOME/.config/${c}
 done
-
-# zsh configuration
-touch $HOME/.zshrc
-if ( ! $(cat $HOME/.zshrc | grep "$PWD/zshrc" > /dev/null) ); then
-  echo "source $PWD/zshrc # Personal" | cat - ~/.zshrc > temp && mv temp ~/.zshrc
-fi
-
-# install Nix (2.8.1) for multi user (for Mac only)
-if [ ! -x "$(command -v nix)" ]; then
-  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.8.1/install)" --daemon
-fi
-
-# install Home manager if not yet installed
-if [ ! -x "$(command -v home-manager)" ]; then
-  mkdir -p /nix/var/nix/profiles/per-user/root/channels
-  nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
-  nix-channel --update
-  export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
-  nix-shell '<home-manager>' -A install
-fi
 
 home-manager switch
