595239518b2030c0025ad460ea1d95c0e2305cf6
Test <me@namnd.com>
2023-01-25 09:38:04 +1100

    remove direnv

--- host/nixpkgs/home.nix
+++ host/nixpkgs/home.nix
@@ -31,18 +31,5 @@
       ssh = "kitty +kitten ssh -R 2489:127.0.0.1:2489";
     };
   };
-
-  programs.direnv = {
-    enable = true;
-    config = {
-      global = {
-        warn_timeout = "10m";
-      };
-    };
-    nix-direnv = {
-      enable = true;
-    };
-  };
-
 }
 
