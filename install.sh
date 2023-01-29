e19c276f4132fee4f8bc51fcd72db8f8b6ef75f8
Nam Nguyen <me@namnd.com>
2023-01-29 23:29:03 +1100

    install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ ! -x "$(command -v home-manager)" ]; then
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
+  nix-channel --update
+  export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+  nix-shell '<home-manager>' -A install
+fi
