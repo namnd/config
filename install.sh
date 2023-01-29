b40fef94cfa0b74c42ee1dc41d44bbe08635b317
Nam Nguyen <me@namnd.com>
2023-01-30 07:56:14 +1100

    Revert "downgrade home-manager to 22.05"
    This reverts commit a07c11fb09bb3dc0a77d9da42843399cc7464867.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 sed -i "s/https:\/\/github.com\/namnd\/nixpkgs/git@github.com:namnd\/nixpkgs.git/g" $HOME/.config/nixpkgs/.git/config
 
 if [ ! -x "$(command -v home-manager)" ]; then
-  nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
   nix-channel --update
   export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
   nix-shell '<home-manager>' -A install
