cacbcdb74f71258fe6b546cb32dc9b0d77c4b8e5
Nam Nguyen <me@namnd.com>
2023-01-29 23:41:15 +1100

    tweak install script

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+if [ "$1" ]; then
+  sed -i "s/git_email/$1/g" $HOME/.config/nixpkgs/custom.nix
+fi
+
+sed -i "s/https:\/\/github.com\/namnd\/nixpkgs.git/git@github.com:namnd\/nixpkgs.git/g" $HOME/.config/nixpkgs/.git/config
+
 if [ ! -x "$(command -v home-manager)" ]; then
   nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
   nix-channel --update
   export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
   nix-shell '<home-manager>' -A install
 fi
+
+home-manager switch
