8e1b5453609a3aee7fa5f83cea15dc0ee6554ca8
Nam Nguyen <nam.nguyen@indebted.co>
2023-01-30 07:23:34 +1000

    auto install languages for TS

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -5,6 +5,7 @@ if not has_ts then
 end
 
 ts_config.setup({
+  ensure_installed = { "go", "lua", "vim", "nix", "bash", "terraform", "hcl", "typescript", "tsx", "javascript" },
   highlight = { enable = true },
   indent = {
     enable = true,
