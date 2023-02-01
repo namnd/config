d03f34b6aa524d9dc7b152f0542165b3c6bacc9d
Nam Nguyen <me@namnd.com>
2023-02-01 22:01:28 +1100

    git enable rebase autoStash

--- home.nix
+++ home.nix
@@ -70,6 +70,7 @@ in
       };
       init.defaultBranch = "master";
       pull.rebase = true;
+      rebase.autoStash = true;
       push.default = "current";
       format.pretty = "%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)";
     };
