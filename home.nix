5485a2c6f9d4a68e16d40fa5b96f0279fdb1a861
Nam Nguyen <me@namnd.com>
2023-03-04 17:33:27 +1100

    fix(kitty): ssh kitten

--- home.nix
+++ home.nix
@@ -116,7 +116,7 @@ in
       gL = "_git_log";
       mcd = "f() { mkdir -p $1 && cd $1 }; f";
       v = "aws-vault exec --debug --backend=file --duration=1h";
-      ssh = "ssh -R 2489:127.0.0.1:2489"; # lemonade server
+      ssh = "kitty +kitten ssh -R 2489:127.0.0.1:2489"; # lemonade server
     };
   };
 
