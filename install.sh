fdcc8ee3434511c56d4a9c1fa72dc9ceb33db7be
Nam Nguyen <me@namnd.com>
2023-06-23 15:16:31 +1000

    upgrade nix to 2.16 and home-manager to 23.05

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 echo '{ config, ... } : {}'  > $PWD/custom.nix
 
 if [ ! -x "$(command -v nix)" ]; then
-  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.13.2/install)" --daemon
+  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.16.1/install)" --daemon
+  exit
 fi
 
 if [ ! -x "$(command -v home-manager)" ]; then
-  nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
   export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
   nix-shell '<home-manager>' -A install
