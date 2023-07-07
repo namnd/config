aafc22bb81095fe6a9c7ea2383188d219c29c95d
Nam Nguyen <me@namnd.com>
2023-07-07 10:41:49 +1000

    fix(ufo): remove tab keymap to restore ctrl + i

--- after/plugin/ufo.lua
+++ after/plugin/ufo.lua
@@ -48,4 +48,3 @@ ufo.setup({
 -- folding - Using ufo provider need remap `zR` and `zM`
 vim.keymap.set('n', 'zR', ufo.openAllFolds)
 vim.keymap.set('n', 'zM', ufo.closeAllFolds)
-vim.keymap.set('n', '<Tab>', 'za', { noremap = true })
