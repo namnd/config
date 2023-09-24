1f181f2585e0b4ee5ac012991c105e426c0952a6
Nam Nguyen <me@namnd.com>
2023-09-24 20:23:04 +1000

    feat: update nix 2.18

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-echo '{ config, ... } : {}'  > $PWD/custom.nix
+if [ ! -f "$PWD/custom.nix" ]; then
+  echo '{ config, ... } : {}'  > $PWD/custom.nix
+fi
 
 if [ ! -x "$(command -v nix)" ]; then
-  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.16.1/install)" --daemon
+  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.18.0/install)" --daemon
   exit
 fi
 
 if [ ! -x "$(command -v home-manager)" ]; then
-  nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
-  export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
   nix-shell '<home-manager>' -A install
 fi
 
