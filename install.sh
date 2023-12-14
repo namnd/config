8c9dca27d19d64ec6629b8a1517b06712afdce7c
Nam Nguyen <nam.nguyen@indebted.co>
2023-12-14 20:32:48 +1000

    feat(nix): upgrade v2.19.2

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ ! -f "$PWD/custom.nix" ]; then
 fi
 
 if [ ! -x "$(command -v nix)" ]; then
-  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.18.0/install)" --daemon
+  sh -c "$(curl -L https://releases.nixos.org/nix/nix-2.19.2/install)" --daemon
   exit
 fi
 
