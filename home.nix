169a55c86033153822a72ad0455eee2f76739037
Nam Nguyen <nam.nguyen@indebted.co>
2024-09-27 11:31:55 +1000

    chore(home.nix): new IPs for VMs

--- home.nix
+++ home.nix
@@ -128,7 +128,8 @@ in
       mcd = "f() { mkdir -p $1 && cd $1 }; f";
       v = "aws-vault exec --debug --backend=file --duration=1h";
       ssh = "ssh -R 2489:127.0.0.1:2489"; # lemonade server
-      work = "ssh namnguyen@192.168.71.3";
+      work = "ssh namnguyen@192.168.71.7";
+      work2 = "ssh namnguyen@192.168.71.15";
       psql = "PAGER=\"nvim -c 'set nomod nolist nowrap syntax=sql'\" psql";
     };
   };
