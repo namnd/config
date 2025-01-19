fdaf7544f1d33c193d1791124fe6ff0732c015bc
Nam Nguyen <me@namnd.com>
2025-01-20 07:57:16 +1000

    fix(home-manager): add nixos-unstable channel

--- home-manager/install.sh
+++ home-manager/install.sh
@@ -18,6 +18,7 @@ fi
 
 if [ ! -x "$(command -v home-manager)" ]; then
   nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz home-manager
+  nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
   nix-channel --update
   export NIX_PATH="$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
   nix-shell '<home-manager>' -A install
