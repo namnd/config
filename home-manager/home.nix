b0e02796867b401bf919bc4246b2c1eb49dae734
Nam Nguyen <me@namnd.com>
2025-06-03 21:32:28 +1000

    feat(home-manager): upgrade to 25.05

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,7 +12,7 @@
     };
   };
 
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
   home.packages = with pkgs; [
     fastfetch
 
