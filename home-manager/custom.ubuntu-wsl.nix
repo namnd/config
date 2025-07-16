8cb686a9da2d689866511ca9d08b1ed81f067afa
Nam Nguyen <me@namnd.com>
2025-07-16 13:29:49 +1000

    fix(ubuntu-wsl): no_proxy config

--- home-manager/custom.ubuntu-wsl.nix
+++ home-manager/custom.ubuntu-wsl.nix
@@ -13,8 +13,8 @@
   programs.bash.profileExtra = ''
 export {http,https,ftp}_proxy=$(secret-tool lookup purpose http_proxy location work)
 export {HTTP,HTTPS,FTP}_PROXY=$(secret-tool lookup purpose http_proxy location work)
-export no_proxy="localhost,127.0.0.*,10.*,192.168.*,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,172.21.*,172.22.*,172.23.*,172.24.*,172.25.*,172.26.*,172.27.*,172.28.*,172.29.*,172.30.*,172.31.*,*.*.ap-southeast-2.eks.amazonaws.com"
-export NO_PROXY="localhost,127.0.0.*,10.*,192.168.*,172.16.*,172.17.*,172.18.*,172.19.*,172.20.*,172.21.*,172.22.*,172.23.*,172.24.*,172.25.*,172.26.*,172.27.*,172.28.*,172.29.*,172.30.*,172.31.*,*.*.ap-southeast-2.eks.amazonaws.com"
+export no_proxy="localhost,.ap-southeast-2.eks.amazonaws.com"
+export NO_PROXY="localhost,.ap-southeast-2.eks.amazonaws.com"
   '';
 
   home.packages = with pkgs; [
@@ -24,7 +24,4 @@ export NO_PROXY="localhost,127.0.0.*,10.*,192.168.*,172.16.*,172.17.*,172.18.*,1
 
     libsecret
   ];
-
-  programs.bash.shellAliases.k = "https_proxy= HTTPS_PROXY= kubectl";
-  programs.bash.shellAliases.h = "https_proxy= HTTPS_PROXY= helm";
 }
