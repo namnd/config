48f35ddecf08e7c2448b84fdcd36d3864b7534c4
Nam Nguyen <me@namnd.com>
2025-12-07 11:33:55 +1000

    sign git commit by default in macos

--- home-manager/custom.macos.nix
+++ home-manager/custom.macos.nix
@@ -1,6 +1,14 @@
 { config, ... }:
 {
 
+  programs.git.settings.user.email = "me@namnd.com";
+  programs.git.signing = {
+    key = "54D86DA33E656F30";
+    signByDefault = true;
+  };
+
+  programs.gpg.enable = true;
+
   home.file = {
     ".config/ghostty".source = config.lib.file.mkOutOfStoreSymlink ../ghostty;
     ".config/hammerspoon".source = config.lib.file.mkOutOfStoreSymlink ../hammerspoon;
