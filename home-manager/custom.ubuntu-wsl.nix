87b2a9502a03b654a3baca1b08d89bb9ebeb9003
Nam Nguyen <me@namnd.com>
2026-05-15 06:23:46 +1000

    simplify setup for ubuntu wsl

--- home-manager/custom.ubuntu-wsl.nix
+++ home-manager/custom.ubuntu-wsl.nix
@@ -7,14 +7,16 @@
     };
   };
 
-  # gnome-keyring doesn't work via home-manager https://github.com/nix-community/home-manager/issues/1454, so workaround below
-  # > sudo apt install gnome-keyring
-  # > secret-tool store --label='Work proxy password' purpose http_proxy location work
   programs.bash.profileExtra = ''
-export {http,https,ftp}_proxy=$(secret-tool lookup purpose http_proxy location work)
-export {HTTP,HTTPS,FTP}_PROXY=$(secret-tool lookup purpose http_proxy location work)
-export no_proxy="localhost,.ap-southeast-2.eks.amazonaws.com"
-export NO_PROXY="localhost,.ap-southeast-2.eks.amazonaws.com"
+export ALL_PROXY=http://127.0.0.1:3128
+export HTTP_PROXY=http://127.0.0.1:3128
+export HTTPS_PROXY=http://127.0.0.1:3128
+export FTP_PROXY=http://127.0.0.1:3128
+export all_proxy=http://127.0.0.1:3128
+export http_proxy=http://127.0.0.1:3128
+export https_proxy=http://127.0.0.1:3128
+export ftp_proxy=http://127.0.0.1:3128
+export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
   '';
 
   home.packages = with pkgs; [
--- hosts/ubuntu-wsl/install.sh
+++ hosts/ubuntu-wsl/install.sh
@@ -4,26 +4,18 @@
 # Install Nix
 #################################################
 
-export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-
-curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install -o /tmp/nix-install
-sh /tmp/nix-install --daemon
-rm /tmp/nix-install
-
-root_dir=$(readlink -f "$PWD"/../..)
+#export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
 
 #################################################
 # Install home-manager
 #################################################
 
-mv "$HOME"/.bashrc "$HOME"/.bashrc.bak
-mv "$HOME"/.profile "$HOME"/.profile.bak
-
-mkdir -p "$HOME"/.config
-
-ln -sfn "$root_dir"/home-manager "$HOME"/.config/home-manager
-ln -sf "$root_dir"/home-manager/custom.ubuntu-wsl.nix "$root_dir"/home-manager/custom.nix
-
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+#nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
+
+ln -sf ~/config/home-manager/custom.ubuntu-wsl.nix ~/config/home-manager/custom.nix
+ln -sfn ~/config/home-manager ~/.config/home-manager
+
